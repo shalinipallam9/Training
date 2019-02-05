62bdef741557010e94cf73648fd2944ed39f588e
sri <shalini.dusharla9@gmail.com>
2019-02-04 19:08:07 -0500

    new changes on 2/3/2019

--- struts-app/src/main/java/com/db/struts/app/WelcomeUserAction.java
+++ struts-app/src/main/java/com/db/struts/app/WelcomeUserAction.java
@@ -1,8 +1,17 @@
 package com.db.struts.app;
 
 public class WelcomeUserAction{
+	
+	
+	public String execute() throws Exception {
+		if("sri".equals(getUsername()) && "sai".equals(getPassword()))
+		return "SUCCESS";
+		else return "ERROR";
+	}
 
 	private String username;
+	
+	private String password;
 	 
 	public String getUsername() {
 		return username;
@@ -11,11 +20,19 @@ public class WelcomeUserAction{
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	
 	// all struts logic here
-	public String execute() {
+	/*public String execute() {
 
 		return "SUCCESS";
 
+	}*/
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
--- struts-app/src/main/webapp/User/pages/welcome_user.jsp
+++ struts-app/src/main/webapp/User/pages/welcome_user.jsp
@@ -6,6 +6,7 @@
 <h1>Struts 2 Hello World Example</h1>
 
 <h4>Hello <s:property value="username"/></h4>
+<h4>Hello <s:property value="password"/></h4>
 
 </body>
 </html>
